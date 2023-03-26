81b4959bbf57b03817c774d84f647aab97eb9698
re11n <97107417+re11n@users.noreply.github.com>
2023-03-26 22:48:33 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_1302200003/Program.cs
+++ tpmodul6_1302200003/Program.cs
@@ -1,2 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System.Diagnostics.Contracts;
+
+internal class Program
+{
+    static void Main(String[] args)
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – [Ryan David Siahaan]");
+        video.PrintVideoDetails();
+        video.IncreasePlayCount(1);
+        video.PrintVideoDetails();
+    }
+}
+
+public class SayaTubeVideo
+{
+    private int id;
+    private String title;
+    private int PlayCount;
+
+    public SayaTubeVideo(String judul)
+    {
+
+        Random ids = new Random();
+        this.title = judul;
+        id = ids.Next(0, 100000);
+        this.PlayCount = 0;
+    }
+
+    public void IncreasePlayCount(int i)
+    {
+            PlayCount = PlayCount + i;
+
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine(this.id);
+        Console.WriteLine(this.title);
+        Console.WriteLine(this.PlayCount);
+    }
+}
